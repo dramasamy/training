030fd1005a00a2e0f4a74a8bc490cc204f4b9430
diramasa <dinesh_kumar.ramasamy@nokia.com>
2020-12-30 17:44:56 -0600

    removed kube-bench redundent file

--- kubernetes/cluster-security/00-kube-bench.sh
+++
@@ -1,6 +0,0 @@
-# kube-bench is a Go application that checks whether Kubernetes is deployed securely by running
-# the checks documented in the CIS Kubernetes Benchmark.
-
-# https://github.com/aquasecurity/kube-bench#running-inside-a-container
-
-docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -t aquasec/kube-bench:latest master --version 1.19
