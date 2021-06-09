aceab9f3880185fa7949f2d016a9ace63d97911d
Dinesh Kumar Ramasamy <dineshkumarr.ramasamy@gmail.com>
2021-06-08 22:25:23 -0500

    Argocd session

---
+++ argocd/install.sh
@@ -0,0 +1,11 @@
+# Install
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# Update Service type to access UI
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
+
+# Login to web UI
+# Username: admnin
+# Passwrod: kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
