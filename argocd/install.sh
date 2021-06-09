0e84c6d5e402ed5dd88d5059abba2ff6f54e2db2
Dinesh Kumar Ramasamy <dineshkumarr.ramasamy@gmail.com>
2021-06-08 23:09:42 -0500

    Updated the installation steps

--- argocd/install.sh
+++ argocd/install.sh
@@ -9,3 +9,11 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
 # Username: admnin
 # Passwrod: kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
+# Login to argocd CLI <master:NodePort>
+argocd login 172.16.94.130:31219
+
+# Add git-repo to Argocd (Fork the following repo and use your private key)
+argocd repo add git@github.com:dramasamy/training.git --insecure-ignore-host-key --ssh-private-key-path ~/.ssh/id_rsa
+
+# Create application
+argocd app create nginx -f nginx/apps/nginx-app.yaml
