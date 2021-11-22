af7b9352b56c10aaa957062f24f1302a7a4c5797
Dinesh Kumar Ramasamy <dineshkumarr.ramasamy@gmail.com>
2021-11-22 17:48:46 -0600

    Update: Docker network connect and local reg doc

--- kind/create.sh
+++ kind/create.sh
@@ -11,6 +11,10 @@ if [ "${running}" != 'true' ]; then
     registry:2
 fi
 
+# connect the registry to the cluster network
+# (the network may already be connected)
+docker network connect "kind" "${reg_name}" || true
+
 # create a cluster with the local registry enabled in containerd
 cat <<EOF | kind create cluster --config=-
 kind: Cluster
@@ -24,12 +28,22 @@ nodes:
   extraMounts:
   - hostPath: /home/dramasamy/projects/kind
     containerPath: /data
-- role: worker
-  extraMounts:
-  - hostPath: /home/dramasamy/projects/kind
-    containerPath: /data
 containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."${reg_name}:${reg_port}"]
     endpoint = ["http://${reg_name}:${reg_port}"]
+EOF
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
 EOF
