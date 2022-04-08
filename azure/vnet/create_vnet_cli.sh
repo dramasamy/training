a0dbc03f143d40f6b92a59a74db87117c23f53e9
Dinesh Kumar Ramasamy <dineshkumarr.ramasamy@gmail.com>
2022-04-07 22:33:50 -0500

    Updated vnet to have multiple address prefixes

--- azure/vnet/create_vnet_cli.sh
+++ azure/vnet/create_vnet_cli.sh
@@ -1,39 +1,47 @@
 source ../common/args.sh
 
-VNET_NAME_1=`whoami`-demo-vnet-1
-VNET_NAME_2=`whoami`-demo-vnet-2
-SUBNET_NAME_1=`whoami`-demo-subnet-1
-SUBNET_NAME_2=`whoami`-demo-subnet-2
+VNET_NAME_1=`whoami`-vnet1
+VNET_NAME_2=`whoami`-vnet2
+VNET_NAME_1_SUBNET_NAME_1=`whoami`-vnet1-subnet1
+VNET_NAME_1_SUBNET_NAME_2=`whoami`-vnet1-subnet2
+VNET_NAME_2_SUBNET_NAME_1=`whoami`-vnet2-subnet1
+VNET_NAME_2_SUBNET_NAME_2=`whoami`-vnet2-subnet2
+writevars "VNET_NAME_1=${VNET_NAME_1}"
+writevars "VNET_NAME_2=${VNET_NAME_2}"
+writevars "VNET_NAME_1_SUBNET_NAME_1=${VNET_NAME_1_SUBNET_NAME_1}"
+writevars "VNET_NAME_1_SUBNET_NAME_2=${VNET_NAME_1_SUBNET_NAME_2}"
+writevars "VNET_NAME_2_SUBNET_NAME_1=${VNET_NAME_2_SUBNET_NAME_1}"
+writevars "VNET_NAME_2_SUBNET_NAME_2=${VNET_NAME_2_SUBNET_NAME_2}"
 
-debug "Create vnet $VNET_NAME_1 with subnet $SUBNET_NAME_1"
+debug "Create vnet $VNET_NAME_1 with subnet $VNET_NAME_1_SUBNET_NAME_1"
 az network vnet create \
    -g $RESOURCE_GROUP \
    -n $VNET_NAME_1 \
-   --address-prefix 10.0.0.0/16 \
-   --subnet-name $SUBNET_NAME_1 \
-   --subnet-prefix 10.0.0.0/24
+   --address-prefixes 10.16.0.0/16 172.16.0.0/16 \
+   --subnet-name $VNET_NAME_1_SUBNET_NAME_1 \
+   --subnet-prefix 10.16.1.0/24
 
-debug "Create subnet $SUBNET_NAME_2 and add it to vnet $VNET_NAME_1"
+debug "Create subnet $VNET_NAME_1_SUBNET_NAME_2 and add it to vnet $VNET_NAME_1"
 az network vnet subnet create \
    -g $RESOURCE_GROUP \
-   -n $SUBNET_NAME_2 \
+   -n $VNET_NAME_1_SUBNET_NAME_2 \
    --vnet-name $VNET_NAME_1 \
-   --address-prefixes "10.0.1.0/24"
+   --address-prefixes 172.16.1.0/24
 
-debug "Create vnet $VNET_NAME_2 with subnet $SUBNET_NAME_1"
+debug "Create vnet $VNET_NAME_2 with subnet $VNET_NAME_2_SUBNET_NAME_1"
 az network vnet create \
    -g $RESOURCE_GROUP \
    -n $VNET_NAME_2 \
-   --address-prefix 10.1.0.0/16 \
-   --subnet-name $SUBNET_NAME_1 \
-   --subnet-prefix 10.1.0.0/24
+   --address-prefix 10.17.0.0/16 172.17.0.0/16 \
+   --subnet-name $VNET_NAME_2_SUBNET_NAME_1 \
+   --subnet-prefix 10.17.1.0/24
+
+debug "Create subnet $VNET_NAME_2_SUBNET_NAME_2 and add it to vnet $VNET_NAME_2"
+az network vnet subnet create \
+   -g $RESOURCE_GROUP \
+   -n $VNET_NAME_2_SUBNET_NAME_2 \
+   --vnet-name $VNET_NAME_2 \
+   --address-prefixes  172.17.1.0/24
 
 debug "list available vnets"
 az network vnet list -g $RESOURCE_GROUP
-
-debug "Create subnet $SUBNET_NAME_2 and add it to vnet $VNET_NAME_2"
-az network vnet subnet create \
-   -g $RESOURCE_GROUP \
-   -n $SUBNET_NAME_2 \
-   --vnet-name $VNET_NAME_2 \
-   --address-prefixes "10.1.1.0/24"
